5c4714a2955c614799b3731cc9fd130cfb7b2831
Marcin Kalita <rubyconvict@gmail.com>
2018-04-09 12:14:59 +0200

    Improve tests for Question 001

--- question001/code_spec.rb
+++ question001/code_spec.rb
@@ -5,23 +5,23 @@ require './question001/code'
 describe Question001::Code do
   describe '.call' do
     it 'with 9 returns 2' do
-      expect(described_class.call(9)).to equal(2)
+      expect(described_class.call(9)).to eq(2)
     end
 
     it 'with 15 returns 0' do
-      expect(described_class.call(15)).to equal(0)
+      expect(described_class.call(15)).to eq(0)
     end
 
     it 'with 20 returns 1' do
-      expect(described_class.call(20)).to equal(1)
+      expect(described_class.call(20)).to eq(1)
     end
 
     it 'with 529 returns 4' do
-      expect(described_class.call(529)).to equal(4)
+      expect(described_class.call(529)).to eq(4)
     end
 
     it 'with 1041 returns 5' do
-      expect(described_class.call(1041)).to equal(5)
+      expect(described_class.call(1041)).to eq(5)
     end
   end
 end
